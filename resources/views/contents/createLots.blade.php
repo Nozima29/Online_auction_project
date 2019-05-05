af19bfef1428bbe7202e60b07e971b6ee05a2274
Darle19 <33263705+Darle19@users.noreply.github.com>
2019-05-05 20:54:19 +0500

    validation all done

--- resources/views/contents/createLots.blade.php
+++ resources/views/contents/createLots.blade.php
@@ -1,85 +1,22 @@
-<script>
-	function validateForm(form){
-		
-
-		fail = validateTitle(form.title.value);
-		fail += validateDescription(form.description.value);
-		fail += validatePrice(form.price.value);
-		fail += validateCountry(form.country.value);
-		fail += validateCity(form.city.value);
-		fail += validateName(form.name.value);
-		fail += validateMail(form.mail.value);
-		fail += validatePhone(form.phone.value);
-		fail += validateAddress(form.address.value);
-
-		if (fail == "") {
-			return true;
-		}else{
-			alert(fail);
-			return false;
-		}
-	}
-
-
-
-	function validateTitle(field) {
-		if (field.length < 5) {
-			 return "Title must be at least 5 characters";		
-
-		}
-	}
-
-	function validatePrice(field) {
-		if ("/[0-9]/".test(field)) 
-			return "Write only in numbers!";		
-	}
-
-	function validateCountry(field) {
-		if (!("/[,2][a-zA-Z]/".test(field)) )
-			return "Country must be at least 2 characters!";		
-	}
-
-	function validateCity(field) {
-		if (!("/[,5][a-zA-Z]/".test(field)) )
-			return "City must be te least 5 characters!";		
-	}
-
-	function validateName(field) {
-		if (!("/[a-zA-Z]/".test(field)) ) 
-			return "Please enter real name!";			
-	}
-
-	function validateMail(field) {
-		if (!((field.indexOf(".") > 0) && (field.indexOf("@") > 0)) || "/[^a-zA-Z0-9.@_-]/".test(field))
-			return "Invalid email address!";
-	}
-
-	function validatePhone(field) {
-		if (!("/[^+0-9][9,11]/".test(field)) ) {
-			return "Please enter valid number with country code!";
-		}
-	}
-	
-</script>
-
 
 @extends('layouts.main')
 @section('content')
+<br><br><br>
+@include ('partials.errors')
 <body class="create_body">
-
 	<div class="container" style="padding: 50px;">
 		<h2 class="text-center">Create New Lots</h2>
 			<div class="form-group">
-	            <form action="/createLots" onsubmit="return validateForm(this)" method="get">
+	            <form action="{{ route('contents.createLots') }}"  method="POST">
 	            	<legend><span>Auction Information</span></legend>
 	            	
 		            <label for="category_name" class="col-sm-8 control-label">Category</label>
-		            <div class="col-sm-8">
-		                <select class="form-control select2" name="category" required>
-		                    <option value="">Categories</option>
-		                    <option value="">Jewellaries</option>
-		                </select>
-		            </div>
+		          	  <div class="col-sm-8">
+		           	     <select class="form-control select2" name="category" required>
+		            	        <option value="">Categories</option>
+		                	    <option value="">Jewellaries</option>
+		                 </select>
+		              </div>
 		            
 		            <label for="title" class="col-sm-8 control-label">Auction Title</label>
 		            <div class="col-sm-8">
@@ -139,21 +76,21 @@
 							<legend>Client Info</legend>
 							<label for="title" class="col-sm-8 control-label">Full Name:</label>
 					            <div class="col-sm-8">
-					                <input type="text" class="form-control" name="name" required>
+					                <input type="text" class="form-control" name="client-name" required>
 					            </div>
 					        <label for="title" class="col-sm-8 control-label">E-mail: </label>
 					            <div class="col-sm-8">
-					                <input type="text" class="form-control" name="mail" placeholder="example.12@gmail.com" required>
+					                <input type="text" class="form-control" name="client-mail" placeholder="example-12@gmail.com" required>
 				            </div>				
 
 				            <label for="title" class="col-sm-8 control-label">Phone number: </label>
 					            <div class="col-sm-8">
-					                <input type="text" class="form-control" name="phone" required>
+					                <input type="text" class="form-control" name="client-phone" required>
 				            </div>				
 
 				            <label for="title" class="col-sm-8 control-label">Address: </label>
 					            <div class="col-sm-8">
-					                <input type="text" class="form-control" name="address" required>
+					                <input type="text" class="form-control" name="client-address" required>
 				            </div>				
 						</div>
 				</div>                    
@@ -179,6 +116,7 @@
 					</div>
 		<hr>
 		<div class="col-sm-12" style="padding: 10px;">
+			@csrf
 			<button class="btn btn-success" type="submit">Save Post</button>
 			<button class="btn btn-success" type="reset">Reset</button>
 
